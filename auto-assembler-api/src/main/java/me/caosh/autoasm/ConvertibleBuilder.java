4d1169c6b0ff7b739738b59445e5a0448ca737df
shuhaoc <shuhaoc@qq.com>
2018-02-03 22:28:55 +0800

    doc(location) : 修正 ConvertibleBuilder 注释

--- auto-assembler-api/src/main/java/me/caosh/autoasm/ConvertibleBuilder.java
+++ auto-assembler-api/src/main/java/me/caosh/autoasm/ConvertibleBuilder.java
@@ -1,14 +1,14 @@
 package me.caosh.autoasm;
 
 /**
- * 适用于{@link AutoAssembler}的Builder接口
+ * 适用于AutoAssembler的Builder接口
  *
  * @author shuhaoc@qq.com
  * @date 2018/1/14
  */
 public interface ConvertibleBuilder<T> {
     /**
-     * 构建对象，由{@link AutoAssembler}调用
+     * 构建对象，由AutoAssembler调用
      *
      * @return 对象实例
      */
